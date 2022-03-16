a969cf8c44152efde4950060aa75a0696e712fe9
Dsanabria02 <dianasanabriac@estudiantec.cr>
2022-03-16 17:30:03 -0600

    Create Figura.java

---
+++ Figura.java
@@ -0,0 +1,16 @@
+
+package tarea.corta.pkg3;
+
+/**
+ *
+ * @author Diana
+ */
+public abstract class Figura{
+    public abstract double calcularArea();
+    
+    
+    public abstract int getCantidadLados();
+    //return 
+    
+}
+    
